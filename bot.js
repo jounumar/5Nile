4a9108714e2328cbab4b5795cbaf7d49c6aa4022
jounumar <44790212+jounumar@users.noreply.github.com>
2019-01-10 04:28:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,7 +48,21 @@ client.on('ready', () => {
   console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
 });
  
- 
+client.on('message', msg => {
+
+    if (msg.content == '5join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => {
+    client.channels.get("530079053789528074").join(); 
+    });
+
 client.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
  
 client.on('reconnecting', () => console.log('I am reconnecting now!'));
